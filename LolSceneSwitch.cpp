218b5fb4bf98edf46fa9bf12d8a84ac44cd3b866
kugelrund <marcel.jacobse@ewetel.net>
2015-06-05 12:44:13 +0200

    Updated addresses for new patch

--- LolSceneSwitch.cpp
+++ LolSceneSwitch.cpp
@@ -40,7 +40,7 @@ bool LoadPlugin()
 
 	InitLog(OBSGetPluginDataPath());
 	plugin = new LolSceneSwitch();
-	Log("INFO  | Loaded LolSceneSwitch v0.2 Alpha 7");
+	Log("INFO  | Loaded LolSceneSwitch v0.2 Alpha 8");
 	return true;
 }
 
@@ -390,8 +390,8 @@ DWORD WINAPI LolSceneSwitch::MonitorThread(_In_ LPVOID lpParam)
 
 	// Stuff for reading memory
 	PointerPath32 clienStatePointer({ -0x658, 0x78, 0x610, 0x4, 0xe48 });
-	PointerPath32 map1Pointer({ -0x994, 0x20 });
-	PointerPath32 map2Pointer({ -0x990, 0x20 });
+	PointerPath32 map1Pointer({ -0x9C0, 0x20 });
+	PointerPath32 map2Pointer({ -0x9C4, 0x20 });
 	DWORD clientStateAddress = 0;
 	long clientState = 0;
 	DWORD mapAddress = 0;
